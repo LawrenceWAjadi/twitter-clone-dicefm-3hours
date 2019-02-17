dc9043cffca7dcb8aebe8fcb1e4e6d6643bbdab9
Lawrence W Ajadi <lwajadi+git@gmail.com>
2019-02-17 23:35:22 +0000

    Setup basic apollo + router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import React, { Component } from "react";
+import ApolloClient, { gql } from "apollo-boost";
 import "./App.css";
 
+const client = new ApolloClient({
+  uri: "https://www.graphqlhub.com/graphql"
+});
+
+const GET_TWITTER_USER = gql`
+  query($identity: UserIdentity!) {
+    graphQLHub
+    twitter {
+      user(identifier: name, identity: $identity) {
+        created_at
+        description
+        id
+        screen_name
+        name
+        profile_image_url
+        url
+        tweets_count
+        followers_count
+        tweets(limit: 50) {
+          text
+          created_at
+          user {
+            screen_name
+            name
+            profile_image_url
+          }
+        }
+      }
+    }
+  }
+`;
+
 class App extends Component {
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+    this.loader();
+  }
+
+  loader() {
+    client
+      .query({
+        query: GET_TWITTER_USER,
+        variables: {
+          identity: this.props.match.params.identity
+        }
+      })
+      .then(result => {
+        console.log(result.data);
+        this.setState({ data: result.data });
+      });
+  }
+
   render() {
     return <div>Hello</div>;
   }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const AppContainer = AppComponent => (
+  <Router>
+    <Route
+      exact
+      path="/:identity"
+      render={({ match }) => <AppComponent match={match} />}
+    />
+  </Router>
+);
+
+ReactDOM.render(AppContainer(App), document.getElementById("root"));
